83ecdb26c655e90ee996575565313cccd9748906
Andy McWilliams <andy@jahya.net>
2015-04-02 15:03:50 -0400

    Fixes lint and jshint warnings

--- app/javascript/site.js
+++ app/javascript/site.js
@@ -1,35 +1,39 @@
 $(document).ready(function() {
-	enableMenuButton();
-	enableSocialLinks();
+	function init() {
+		enableMenuButton();
+		enableSocialLinks();
+	}
+
+	function enableMenuButton() {
+		var menuOverlaid = false;
+
+		$('#menu-button').click(function() {
+			if(menuOverlaid){
+				$('header.main>nav').removeClass('popout');
+			} else {
+				$('header.main>nav').addClass('popout');
+			}
+			menuOverlaid = !menuOverlaid;
+		});
+	}
+
+	function enableSocialLinks() {
+		$('.social a').click(function(event) {
+			var width =  575,
+			    height = 400,
+				left =   ($(window).width()	- width) / 2,
+				top	 =   ($(window).height() - height) / 2,
+				url	=    this.href,
+				opts =   'status=1' +
+						 ',width='	+ width	+
+						 ',height=' + height +
+						 ',top='	+ top +
+						 ',left='	+ left;
+
+			window.open(url, 'social-share', opts);
+			event.preventDefault();
+		});
+	}
+
+	init();
 });
-
-function enableMenuButton() {
-	var menuOverlaid = false;
-
-	$('#menu-button').click(function() {
-		if(menuOverlaid){
-			$('header.main>nav').removeClass('popout');
-		} else {
-			$('header.main>nav').addClass('popout');
-		}
-		menuOverlaid = !menuOverlaid;
-	});
-}
-
-function enableSocialLinks() {
-	$('.social a').click(function(event) {
-		var width =  575,
-		    height = 400,
-			left =   ($(window).width()	- width) / 2,
-			top	 =   ($(window).height() - height) / 2,
-			url	=    this.href,
-			opts =   'status=1' +
-					 ',width='	+ width	+
-					 ',height=' + height +
-					 ',top='	+ top +
-					 ',left='	+ left;
-
-		window.open(url, 'social-share', opts);
-		event.preventDefault();
-	});
-}
