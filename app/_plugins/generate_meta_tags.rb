fef8af6e3bc8b2ee31e24b197fe769e1053c5ae1
Andrew McWilliams <andy@jahya.net>
2018-04-27 19:16:05 -0400

    Generates meta tags for projects

--- app/_plugins/generate_meta_tags.rb
+++ app/_plugins/generate_meta_tags.rb
@@ -5,15 +5,17 @@ module GenerateMetaTags
     def generate(site)
       begin_benchmark
       load_regexes
-      load_prefixes
-      generate_meta_tags site
+      load_prefixes "/images/projects"
+      generate_meta_tags site.collections["projects"]
+      load_prefixes "/images/posts"
+      generate_meta_tags site.collections["posts"]
       end_benchmark
     end
 
     def begin_benchmark
       @start = Time.now
       puts
-      puts "Generating image and description meta tags on posts..."
+      puts "Generating image and description meta tags on projects and blog posts..."
     end
 
     def end_benchmark
@@ -24,74 +26,104 @@ module GenerateMetaTags
 
     def load_regexes
       @img_tag_regex = /img.*?src="(.*?)"/i
+      @img_md_regex = /\{%.*?include image.html.*?file=\'(.*?)\'.*?/i
+
       @vimeo_tag_regex = /player.vimeo.com\/video\/(.*?)\?/i
+      @vimeo_md_regex = /\{%.*?include vimeo.html.*?id=\'(.*?)\'.*?/i
+
       @youtube_tag_regex = /youtube.com\/embed\/(.*?)"/i
+      @youtube_md_regex = /\{%.*?include youtube.html.*?id=\'(.*?)\'.*?/i
+
       @paragraph_tag_regex = /<p>(.*)<\/p>/i
       @paragraph_wo_tag_regex = /^(?!<[a-z])(.*)\n/i
     end
 
-    def load_prefixes
+    def load_prefixes(img_prefix)
+      @img_url_prefix = img_prefix
       @vimeo_url_prefix = "http://vimeo.com/"
       @youtube_url_prefix = "http://www.youtube.com/watch?v="
     end
 
-    def generate_meta_tags(site)
-      site.posts.docs.each do |post|
-        if post.data['image'].nil? then
-          generate_image_tag(post)
+    def generate_meta_tags(collection)
+      collection.docs.each do |item|
+        if item.data['image'].nil? then
+          generate_image_tag(item)
         end
-        if post.data['description'].nil? then
-          generate_description_tag(post)
+        if item.data['description'].nil? then
+          generate_description_tag(item)
         end
       end
     end
 
-    def generate_image_tag(post)
-      image_url = get_image_url(post.content)
+    def generate_image_tag(item)
+      image_url = get_image_url(item)
 
       if is_usable(image_url) then
-        post.data['image'] = image_url
+        item.data['image'] = image_url
       end
     end
 
-    def generate_description_tag(post)
-      first_paragraph = get_first_paragraph(post.content)
+    def generate_description_tag(item)
+      first_paragraph = get_first_paragraph(item.content)
 
       if is_usable(first_paragraph) then
         first_paragraph = strip_tags(first_paragraph)
-        post.data['description'] = first_paragraph
+        item.data['description'] = first_paragraph
       end
     end
 
-    def get_image_url(content)
-      type = discover_which_image_type(content)
+    def get_image_url(item)
+      type = discover_which_image_type(item.content)
       case type
       when :img
-        get_img_tag_url(content)
+        get_img_tag_url(item)
+      when :img_md
+        get_img_md_url(item)
       when :vimeo
-        get_vimeo_thumb_url(content)
+        get_vimeo_thumb_url(item)
+      when :vimeo_md
+        get_vimeo_md_url(item)
       when :youtube
-        get_youtube_thumb_url(content)
+        get_youtube_thumb_url(item)
+      when :youtube_md
+        get_youtube_md_url(item)
       end
     end
 
-    def get_img_tag_url(content)
-      content[@img_tag_regex, 1]
+    def get_img_tag_url(item)
+      item.content[@img_tag_regex, 1]
     end
 
-    def get_vimeo_thumb_url(content)
-      id = content[@vimeo_tag_regex, 1]
+    def get_img_md_url(item)
+      file = item.content[@img_md_regex, 1]
+      itempath = item.path.split('/')[-1]
+      itempath = itempath.split('.')[0]
+      "#{@img_url_prefix}/#{itempath}/#{file}"
+    end
+
+    def get_vimeo_thumb_url(item)
+      id = item.content[@vimeo_tag_regex, 1]
       VideoThumb::get("#{@vimeo_url_prefix}#{id}", "max") || nil
     end
 
-    def get_youtube_thumb_url(content)
-      id = content[@youtube_tag_regex, 1]
+    def get_vimeo_md_url(item)
+      id = item.content[@vimeo_md_regex, 1]
+      VideoThumb::get("#{@vimeo_url_prefix}#{id}", "max") || nil
+    end
+
+    def get_youtube_thumb_url(item)
+      id = item.content[@youtube_tag_regex, 1]
+      VideoThumb::get("#{@youtube_url_prefix}#{id}", "max") || nil
+    end
+
+    def get_youtube_md_url(item)
+      id = item.content[@youtube_md_regex, 1]
       VideoThumb::get("#{@youtube_url_prefix}#{id}", "max") || nil
     end
 
     def get_first_paragraph(content)
-      type = discover_which_paragraph_type(content)
-      case type
+      @paragraph_type = discover_which_paragraph_type(content)
+      case @paragraph_type
       when :with_tag
         content[@paragraph_tag_regex, 1]
       when :without_tag
@@ -100,14 +132,23 @@ module GenerateMetaTags
     end
 
     def strip_tags(content)
-      content.gsub(/<\/?[^>]*>/, "")
+      rt = content.gsub(/<\/?[^>]*>/, "")
+      if @paragraph_type == :without_tag
+        rt = rt.gsub(/\(\/?[^>]*\)/, "")
+        rt = rt.tr("[", "")
+        rt = rt.tr("]", "")
+      end
+      rt
     end
 
     def discover_which_image_type(content)
       potentials = {
         img: find_first_position_of(@img_tag_regex, content),
+        img_md: find_first_position_of(@img_md_regex, content),
         vimeo: find_first_position_of(@vimeo_tag_regex, content),
-        youtube: find_first_position_of(@youtube_tag_regex, content)
+        vimeo_md: find_first_position_of(@vimeo_md_regex, content),
+        youtube: find_first_position_of(@youtube_tag_regex, content),
+        youtube_md: find_first_position_of(@youtube_md_regex, content)
       }
       (potentials.min_by {|k,v| v || 1000000}).first
     end
@@ -125,7 +166,7 @@ module GenerateMetaTags
     end
 
     def find_first_position_of(needle, haystack)
-      (haystack.enum_for(:scan, needle).map { Regexp.last_match.begin(0) }).first
+      haystack =~ needle
     end
   end
 end
